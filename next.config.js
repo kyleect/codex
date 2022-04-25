9e2ae878e66de11781ed8d6f41feaf16835ef87e
Kylee Tilley <kyleetilley@gmail.com>
2022-04-25 13:48:41 -0500

    Add basePath

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/codex'
 }
 
 module.exports = withContentlayer(nextConfig);
