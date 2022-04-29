3c5254aee401951e268c848b59756a53cef6b9df
Kylee Tilley <kyleetilley@gmail.com>
2022-04-28 22:33:54 -0500

    Add residents to location pages

--- pages/locations/[slug].tsx
+++ pages/locations/[slug].tsx
@@ -1,5 +1,6 @@
-import { Heading } from "@chakra-ui/react";
-import { allLocations, Location as ViewLocation } from "contentlayer/generated";
+import { Box, Flex, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
+import BeingLink from "components/BeingLink";
+import { allLocations, allBeings, Location } from "contentlayer/generated";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { mdxComponents } from "src";
 
@@ -23,22 +24,48 @@ export async function getStaticProps({ params }) {
 }
 
 type Props = {
-  location: ViewLocation;
+  location: Location;
 };
 
 const ViewLocation = ({ location }: Props) => {
   const MDX = useMDXComponent(location.body.code);
 
+  const residents = allBeings.filter(
+    (being) => being.placeOfResidence === location.name
+  );
+
   return (
     <>
-      <Heading marginBottom={5}>Locations</Heading>
-
       <article>
-        <Heading as="h3" marginBottom={5}>
-          {location.name}
-        </Heading>
+        <Flex>
+          <Box py={3} flexGrow={4}>
+            <Heading as="h3" marginBottom={5}>
+              {location.name}
+            </Heading>
 
-        <MDX components={mdxComponents} />
+            <MDX components={mdxComponents} />
+          </Box>
+
+          <Box py={3} mx={5} flexGrow={1}>
+            {residents.length > 0 && (
+              <Box mb={5}>
+                <Heading as="h4" size="lg" pb={5}>
+                  Residents
+                </Heading>
+
+                <UnorderedList>
+                  {residents.map((resident, i) => {
+                    return (
+                      <ListItem key={i}>
+                        <BeingLink name={resident.name} />
+                      </ListItem>
+                    );
+                  })}
+                </UnorderedList>
+              </Box>
+            )}
+          </Box>
+        </Flex>
       </article>
     </>
   );
